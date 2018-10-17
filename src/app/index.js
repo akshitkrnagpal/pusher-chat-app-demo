540ac839092d1113ee482ba4e69333b9b7f6dc4d
akshitkrnagpal <akshitkrnagpal@gmail.com>
2018-10-17 23:17:11 +0530

    Add Semantic UI

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div/>
+      <Container />
     );
   }
 }
