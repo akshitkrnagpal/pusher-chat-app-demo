bf87d9008643f5638fa335fa21d5323f84f8b633
akshitkrnagpal <akshitkrnagpal@gmail.com>
2018-10-18 18:42:46 +0530

    Add functionality Send Message to API route

--- src/app/components/SendMessage.js
+++ src/app/components/SendMessage.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
-import { Input } from 'semantic-ui-react';
+import { Button, Form } from 'semantic-ui-react';
+import axios from 'axios';
 
 class SendMessage extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            message: ''
+        }
+
+        this._onChange = this._onChange.bind(this);
+        this._onSubmit = this._onSubmit.bind(this);
+        this._clearInput = this._clearInput.bind(this);
+    }
     render() {
         return (
-            <Input fluid placeholder = 'Enter Message' action = 'Send' />
+            <Form>
+                <Form.Input
+                    fluid
+                    placeholder = 'Enter Message'
+                    action = {
+                        <Button onClick = { this._onSubmit } value = 'Send' />
+                    }
+                    value = { this.state.message }
+                    onChange = { this._onChange }
+                />
+            </Form>
         );
+    }
+
+    _onChange(event) {
+        this.setState({
+            message: event.target.value
+        });
+    }
+
+    _onSubmit() {
+        const { message } = this.state;
+        if(!message || 0 === message.length) {
+            return;
+        }
+
+        axios.post('http://localhost:3002/message', {
+            username: 'akshitkrnagpal',
+            message: this.state.message
+        }).then( this._clearInput ).catch( function(error) {
+            console.log(error)
+        });
+    }
+
+    _clearInput() {
+        this.setState({
+            message: ''
+        });
     }
 }
 
