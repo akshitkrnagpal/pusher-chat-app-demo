9464e64363c77004a898c033c22db3388f391b88
akshitkrnagpal <akshitkrnagpal@gmail.com>
2018-10-18 17:02:38 +0530

    Replaced Messages with React Virtualized List

--- src/app/components/Messages.js
+++ src/app/components/Messages.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
-import { Segment } from 'semantic-ui-react';
+import { Comment } from 'semantic-ui-react';
+
+import { WindowScroller, List } from 'react-virtualized'
+
+const data = [
+    {
+        'username': 'akshitkrnagpal',
+        'message': 'Hi. My name is Akshit'
+    },
+    {
+        'username': 'akshit1708',
+        'message': 'Hi Akshit. My name is also Akshit'
+    }
+];
 
 class Messages extends Component {
+    constructor() {
+        super();
+
+        this._messageRenderer = this._messageRenderer.bind(this);
+    }
+
     render() {
         return (
-            <div>
-                <Segment vertical> First Message </Segment>
-                <Segment vertical> Second Message </Segment>
-                <Segment vertical> Third Message </Segment>
-            </div>
+            <Comment.Group>
+                <WindowScroller>
+                {
+                    (props) => (
+                        <List
+                            autoHeight
+                            width = { props.width }
+                            height = { props.height }
+                            rowCount = { data.length }
+                            rowHeight = { 70 }
+                            rowRenderer = { this._messageRenderer }
+                        />
+                    )
+                }
+                </WindowScroller>
+            </Comment.Group>
+        );
+    }
+
+    _messageRenderer(props) {
+        const { username, message } = data[props.index];
+
+        return (
+            <Comment key = { props.key } style = { props.style }>
+                <Comment.Content>
+                    <Comment.Author> { username } </Comment.Author>
+                    <Comment.Text> { message } </Comment.Text>
+                </Comment.Content>
+            </Comment>
         );
     }
 }
